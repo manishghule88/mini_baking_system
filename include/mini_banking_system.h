9268e547e04ad27966174cb5038214d31d5ef455
manishghule88 <manishghule26@gmail.com>
2026-03-20 16:59:54 +0530

    dev: add declare findAccountInFile method

--- include/mini_banking_system.h
+++ include/mini_banking_system.h
@@ -1,6 +1,8 @@
 #ifndef MINI_BANKING_SYSTEM_H
 #define MINI_BANKING_SYSTEM_H
 
+#include <fstream>
+
 class BankingSystem {
 
     public:
@@ -12,12 +14,14 @@ class BankingSystem {
         int mAcceptInput = 0;
         int mInitialAmount = 0;
         std::string mName;
+        std::ofstream mFile;
         std::string accountsFileName = "accounts.dat";
 
         void showMenu();
         void createAccout();
         double uniqueNumberGeneration();
         void depositMoney();
+        bool findAccountInFile( double aExistAccountNumber );
 };
 
 #endif
