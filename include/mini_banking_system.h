a4ddb90a37cf3a49b70ca7e24efc757b80435e73
manishghule88 <manishghule26@gmail.com>
2026-03-20 17:55:02 +0530

    dev: add documentation of some methods

--- include/mini_banking_system.h
+++ include/mini_banking_system.h
@@ -17,10 +17,39 @@ class BankingSystem {
         std::ofstream mFile;
         std::string accountsFileName = "accounts.dat";
 
+        /// Displays the banking menu and processes user input
+        /// The user to select and action and calls corresponding
+        /// function based on the selected option.
         void showMenu();
+
+        /// Creates a new bank account with user details/
+        /// Collects user name and initial deposit amount validates
+        /// the minimum requirement, generates a unique account
+        /// number and stores the account details in a file.
         void createAccout();
+
+        /// Generates a unique account number.
+        /// Reads the account file extracts the last valid account
+        /// number and returns a new incremented account number.
+        /// @return double
+        ///     Newly generated unique account number.
         double uniqueNumberGeneration();
+
+        /// Deposits money into an existing bank account.
+        /// Verifies the account number, accepts a deposit amount,
+        /// updates the account balance in the file, and rewrites the
+        /// updated data using a temporary file.
+        ///
         void depositMoney();
+        
+        /// Checks whether an account exists in the file.
+        /// Reads the accounts file line by line extracts the account
+        /// number, and compares it with the given account number.
+        ///
+        /// @param[ in ] aExistAccountNumber
+        ///          Account number to search for
+        /// @return true
+        ///     true if account is found false otherwise
         bool findAccountInFile( double aExistAccountNumber );
 };
 
